b554220b5db6aea918fff92f5fba268b2f902bb7
Gobinath <slgobinath@gmail.com>
2015-11-15 12:50:08 +0530

    Icon is changed and Spalsh screen is added.

--- app/src/main/java/com/aslan/contra/view/activity/SplashActivity.java
+++ app/src/main/java/com/aslan/contra/view/activity/SplashActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.aslan.contra.R;
 import com.aslan.contra.util.Constants;
@@ -15,12 +17,21 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove title
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
 
         // Show the splash screen
         new Thread() {
             @Override
             public void run() {
+                try {
+                    Thread.sleep(Constants.SPLASH_VISIBLE_TIME);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, e.getMessage());
+                }
                 boolean firstRun = Utility.isFirstRun(getApplicationContext());
                 Log.i("Splash Activity", "" + firstRun);
                 Class target;
