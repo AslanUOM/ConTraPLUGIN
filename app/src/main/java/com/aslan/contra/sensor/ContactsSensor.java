3a97556e766c9bb886d2797c31390fb51af61645
vishnuvathsan <vishnuvathsan@gmail.com>
2015-12-25 15:24:44 +0530

    Contact Sensor updated to collect all the stored phone numbers

--- app/src/main/java/com/aslan/contra/sensor/ContactsSensor.java
+++ app/src/main/java/com/aslan/contra/sensor/ContactsSensor.java
@@ -4,7 +4,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,35 +13,95 @@ import java.util.List;
  */
 public class ContactsSensor {
     private Context context;
+    private List<String> contacts;
+    private ContentResolver cr;
 
     public ContactsSensor(Context context) {
         this.context = context;
+        this.contacts = new ArrayList<>();
+        this.cr = context.getContentResolver();
     }
 
     public List<String> collect() {
-        List<String> contacts = new ArrayList<>();
-        ContentResolver cr = context.getContentResolver();
-        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        Log.i("No of contacts", "" + cursor.getCount());
-        if (cursor.moveToFirst()) {
+//        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+//        Log.i("No of contacts", "" + cursor.getCount());
+//        if (cursor.moveToFirst()) {
+//
+//            do {
+//                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//
+//                if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+//                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+//                    while (pCur.moveToNext()) {
+//                        String contactNumber = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                        contacts.add(contactNumber);
+//                        break;
+//                    }
+//                    pCur.close();
+//                }
+//
+//            } while (cursor.moveToNext());
+//        }
+//        cursor.close();
 
-            do {
-                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-
-                if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-                    Cursor pCur = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
-                    while (pCur.moveToNext()) {
-                        String contactNumber = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                        contacts.add(contactNumber);
-                        break;
-                    }
-                    pCur.close();
-                }
-
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-
+        queryAllRawContacts();
         return contacts;
+    }
+
+    private void queryAllRawContacts() {
+
+        final String[] projection = new String[]{
+                ContactsContract.RawContacts.CONTACT_ID,                    // the contact id column
+                ContactsContract.RawContacts.DELETED                        // column if this contact is deleted
+        };
+
+        final Cursor rawContacts = cr.query(
+                ContactsContract.RawContacts.CONTENT_URI,                // the uri for raw contact provider
+                projection,
+                null,                                    // selection = null, retrieve all entries
+                null,                                    // not required because selection does not contain parameters
+                null);                                    // do not order
+
+        final int contactIdColumnIndex = rawContacts.getColumnIndex(ContactsContract.RawContacts.CONTACT_ID);
+        final int deletedColumnIndex = rawContacts.getColumnIndex(ContactsContract.RawContacts.DELETED);
+
+        if (rawContacts.moveToFirst()) {                    // move the cursor to the first entry
+            while (!rawContacts.isAfterLast()) {            // still a valid entry left?
+                final int contactId = rawContacts.getInt(contactIdColumnIndex);
+                final boolean deleted = (rawContacts.getInt(deletedColumnIndex) == 1);
+                if (!deleted) {
+                    queryAllPhoneNumbersForContact(contactId);
+                }
+                rawContacts.moveToNext();                // move to the next entry
+            }
+        }
+
+        rawContacts.close();
+    }
+
+    public void queryAllPhoneNumbersForContact(int contactId) {
+        final String[] projection = new String[]{
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+//                ContactsContract.CommonDataKinds.Phone.TYPE,
+        };
+
+        final Cursor phone = cr.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection,
+                ContactsContract.Data.CONTACT_ID + "=?",
+                new String[]{String.valueOf(contactId)},
+                null);
+
+        if (phone.moveToFirst()) {
+            final int contactNumberColumnIndex = phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+
+            while (!phone.isAfterLast()) {
+                final String number = phone.getString(contactNumberColumnIndex);
+                contacts.add(number);
+                phone.moveToNext();
+            }
+
+        }
+        phone.close();
     }
 }
